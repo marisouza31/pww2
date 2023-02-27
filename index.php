ead32fd162477b4c2d44d47fd9c98c896a30e961
mariana ocireu <99839123+marisouza31@users.noreply.github.com>
2023-02-27 15:11:44 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+// Criacao de variavel
+$a = 0;
+if($b <= 10){
+    echo "o valor da variavel b é maior ou igual  a 10";
+}else{
+    echo "o valor da variavel b é menor que 10";
+}
+
+//Laços de repetição
+for($i = 0; $i <= 10; $i++){
+    echo $i;
+
+}
+$x = 0;
+while($x <= 10){
+    echo $x;
+    $x++;
+}
+
+$y = 0;
+do{
+    echo $y;
+    $y++;
+}while($y <= 10);
+
+?>
